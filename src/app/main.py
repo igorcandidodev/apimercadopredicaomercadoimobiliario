e51b9f3466fb98dcadfa84b2319e7cc41dd82e2a
igorcandidodev <ygoraveiro@gmail.com>
2024-11-20 21:32:31 -0300

    cors allowed all origins

--- src/app/main.py
+++ src/app/main.py
@@ -5,8 +5,17 @@ from pydantic import BaseModel
 import pandas as pd
 
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI()
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 class Imovel(BaseModel):
     regiao: str
